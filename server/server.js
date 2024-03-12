8fa3b0b20e83f5f79704c608b7079e2098e94407
mattdbracaglia <162937123+mattdbracaglia@users.noreply.github.com>
2024-03-11 22:02:30 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, '..', 'Decksim')));
