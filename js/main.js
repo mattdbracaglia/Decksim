26861d22f60336772fb66b0b956bf2e32ce3cd2e
mattdbracaglia <162937123+mattdbracaglia@users.noreply.github.com>
2024-03-26 17:31:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1413,9 +1413,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 break;
             case 3:
-                console.log("Case 3: Going to the next player's turn.");
-                goToNextPlayer();
-                currentChoicesTurnStep = 0; // Reset to start at step 0 for the next player
+                console.log("Case 3: Preparing to go to the next player's turn.");
+                setTimeout(() => {
+                    console.log("Case 3: Going to the next player's turn.");
+                    goToNextPlayer();
+                    currentChoicesTurnStep = 0; // Reset to start at step 0 for the next player
+                }, 1000); // 1000 milliseconds delay equals 1 second
                 break;
             default:
                 console.log("Unknown step encountered. Resetting to step 0.");
